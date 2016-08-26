f4be133bb639abd16e1234a8fcb7c36d2389c2bf
jcant0n <javiercantonferrero@gmail.com>
2016-08-26 14:12:47 +0200

    Created a new extension

---
+++ HTCVive/HTCVive/WaveEngine.OpenVR.Shared/NativeLibrary.cs
@@ -0,0 +1,74 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// NativeLibraries
+//
+// Copyright © 2016 Wave Engine S.L. All rights reserved.
+// Use is subject to license terms.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using WaveEngine.Common;
+#endregion
+
+namespace WaveEngine.OpenVR.Shared
+{
+    /// <summary>
+    /// This static class is used to load the native libraries.
+    /// </summary>
+    public static class NativeLibraries
+    {
+        [SuppressUnmanagedCodeSecurity, DllImport("kernel32")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        static NativeLibraries()
+        {
+            LoadNativeLibrary("openvr_api.dll");
+        }
+
+        /// <summary>
+        /// Load a native library embedded in the current assembly.
+        /// </summary>
+        /// <param name="fileName">Library name.</param>
+        private static void LoadNativeLibrary(string fileName)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), fileName);
+
+            if (!File.Exists(path) || (LoadLibrary(path) == IntPtr.Zero))
+            {
+                path = Path.Combine(Path.GetTempPath(), fileName);
+
+                try
+                {
+                    Type type = typeof(NativeLibraries);
+
+                    string name;
+
+                    if (Environment.Is64BitProcess)
+                    {
+                        name = type.Namespace + ".Libs.x64." + fileName;
+                    }
+                    else
+                    {
+                        name = type.Namespace + ".Libs.x86." + fileName;
+                    }
+
+                    using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                    {
+                        Assembly.GetExecutingAssembly().GetManifestResourceStream(name).CopyTo(stream);
+                    }
+                }
+                catch
+                {
+                }
+
+                LoadLibrary(path);
+            }
+        }
+    }
+}
---
+++ HTCVive/HTCVive/WaveEngine.OpenVR.Shared/SteamVRService.cs
@@ -0,0 +1,76 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// SteamVRService
+//
+// Copyright © 2016 Wave Engine S.L. All rights reserved.
+// Use is subject to license terms.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using WaveEngine.Common;
+using System.Diagnostics;
+#endregion
+
+namespace WaveEngine.OpenVR.Shared
+{
+    public class SteamVRService : Service
+    {
+        private Valve.VR.CVRSystem hmd;
+
+        #region Initialize
+        public SteamVRService()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+        public Valve.VR.CVRSystem HMD { get; set; }
+        #endregion
+
+        #region Private Methods
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var error = Valve.VR.EVRInitError.None;
+
+            hmd = Valve.VR.OpenVR.Init(ref error);
+            if (error != Valve.VR.EVRInitError.None)
+            {
+                ReportError(error);
+                Valve.VR.OpenVR.Shutdown();
+            }
+        }
+
+        protected override void Terminate()
+        {
+            base.Terminate();
+            Valve.VR.OpenVR.Shutdown();
+        }
+
+        private void ReportError(Valve.VR.EVRInitError error)
+        {
+            switch (error)
+            {
+                case Valve.VR.EVRInitError.None:
+                    break;
+                case Valve.VR.EVRInitError.VendorSpecific_UnableToConnectToOculusRuntime:
+                    Debug.WriteLine("SteamVR Initialization Failed!  Make sure device is on, Oculus runtime is installed, and OVRService_*.exe is running.");
+                    break;
+                case Valve.VR.EVRInitError.Init_VRClientDLLNotFound:
+                    Debug.WriteLine("SteamVR drivers not found!  They can be installed via Steam under Library > Tools.  Visit http://steampowered.com to install Steam.");
+                    break;
+                case Valve.VR.EVRInitError.Driver_RuntimeOutOfDate:
+                    Debug.WriteLine("SteamVR Initialization Failed!  Make sure device's runtime is up to date.");
+                    break;
+                default:
+                    Debug.WriteLine(Valve.VR.OpenVR.GetStringForHmdError(error));
+                    break;
+            }
+        }
+        #endregion
+    }
+}
